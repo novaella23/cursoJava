96b02230e9fc88a4a77ec242888e345c809e46f4
novaella23 <dadderio79@outlook.com>
2023-02-14 21:06:40 -0300

    21h06 14-2-23

--- byteBanco/src/byteBanco/Conta.java
+++ byteBanco/src/byteBanco/Conta.java
@@ -5,4 +5,29 @@ public class Conta {
     int agencia;
     int numero;
     String titular;
+
+    
+    //método deposita:  //void é o retorno
+    void deposita(double valor) {
+	this.saldo += valor;
+    }
+    
+    //método saca:
+    public boolean saca(double valor) {
+    	if(this.saldo>=valor) {
+    		this.saldo-=valor;
+    		return true;
+    	}else {
+    		System.out.println("Saldo insuficiente");
+    		return false;
+    	}
+    }
+   public boolean transfere(double valor, Conta destino) {
+	   if (this.saldo>=valor) {
+		   this.saldo -= valor;
+		   destino.deposita(valor);
+		   return true;
+		}
+	   return false;
+   }
 }
