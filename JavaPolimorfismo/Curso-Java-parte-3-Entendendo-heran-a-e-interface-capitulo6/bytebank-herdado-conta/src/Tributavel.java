4a4a9ea17490b10e8f2184a45fd6272a9759c2da
novaella23 <dadderio79@outlook.com>
2023-04-03 16:23:34 -0300

    03-04

---
+++ JavaPolimorfismo/Curso-Java-parte-3-Entendendo-heran-a-e-interface-capitulo6/bytebank-herdado-conta/src/Tributavel.java
@@ -0,0 +1,8 @@
+//abstract está implícito dentro de interface, não sendo necessário escrever public abstract interface
+
+public interface Tributavel {
+
+	double getValorImposto(); //não tem corpo e também não é necessário colocar public abstract
+
+	
+}
